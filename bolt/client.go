e691011dee2b0cd070fee39f8f8b07d61f19c4e7
Robert G <robertguenzler@gmail.com>
2017-02-22 15:46:29 +0100

    add functions for ouput

--- bolt/client.go
+++ bolt/client.go
@@ -43,6 +43,7 @@ func NewClient() *Client {
 	return &Client{Path: DBPath, Timeout: 1 * time.Second}
 }
 
+
 // Open opens the DB
 func (c *Client) Open() error {
 	// Open the database
@@ -157,4 +158,32 @@ func (c *Client) Put(bucket, key string, value interface{}) error {
 func (c *Client) Stats() string {
 	txNum := c.db.Stats().TxN
 	return fmt.Sprintf("tx_num: %v\n", txNum)
+}
+
+// String returns the string representation of the database
+func (c *Client) String() string {
+	var out string
+
+	var dbReader = func(k []byte, v []byte) error {
+		out = out + "> " + string(k) + " => " + string(v) + "\n"
+		return nil
+	}
+
+	if err := c.db.View(func(tx *bolt.Tx) error {
+		var err error
+
+		out = out + "user bucket:\n\n"
+		ub := tx.Bucket([]byte(UserBucket))
+		err = ub.ForEach(dbReader)
+
+		out = out + "session bucket:\n\n"
+		sb := tx.Bucket([]byte(SessionBucket))
+		err = sb.ForEach(dbReader)
+
+		return err
+	}); err != nil {
+		panic(err)
+	}
+
+	return out
 }
