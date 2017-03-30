95adb9055c637ff1aec20646b1484557f4b8dd11
Robert G <robertguenzler@gmail.com>
2017-03-30 19:05:23 +0200

    wip: use one worker for read/write events

--- pkg/websock/hub.go
+++ pkg/websock/hub.go
@@ -18,9 +18,10 @@ type Hub struct {
 	connectionsMx sync.RWMutex
 
 	// TODO hub-wide message channel?
-	// broadcast chan []byte
+	broadcast chan []byte
 
-	log log.Logger
+	quit chan chan struct{}
+	log  log.Logger
 }
 
 // NewHub creates a new hub
@@ -29,10 +30,23 @@ func NewHub() *Hub {
 		Verbose:       false,
 		connections:   make(map[string]Connection),
 		connectionsMx: sync.RWMutex{},
-		// broadcast:     make(chan []byte, 2048),
-		log: *log.New(os.Stdout, "[websock]", log.LstdFlags),
+		broadcast:     make(chan []byte),
+		quit:          make(chan chan struct{}),
+		log:           *log.New(os.Stdout, "[websock]", log.LstdFlags),
 	}
+	go h.broadcaster()
 	return h
+}
+
+func (h *Hub) BroadcastChan() chan []byte {
+	return h.broadcast
+}
+
+// Close sends the quit signal to the monitor worker
+func (h *Hub) Close() {
+	q := make(chan struct{})
+	h.quit <- q
+	<-q
 }
 
 // AddConnection remembers a connection
@@ -72,9 +86,24 @@ func (h *Hub) GetConnection(id string) (*Connection, error) {
 	return &c, nil
 }
 
-// Broadcast sends a message to all connections
-func (h *Hub) Broadcast(m []byte) {
-	for _, conn := range h.connections {
+// broadcast sends a message to all connections
+func (h *Hub) sendBroadcastMessage(m []byte) {
+	for cID, conn := range h.connections {
+		println("notified:", cID)
 		conn.SendChan() <- m
+	}
+}
+
+func (h *Hub) broadcaster() {
+	println("starting broadcaster worker...")
+	for {
+		select {
+		case msg := <-h.broadcast:
+			println("broadcasting", string(msg))
+			h.sendBroadcastMessage(msg)
+		case q := <-h.quit:
+			close(q)
+			return
+		}
 	}
 }
